f5214176d3d1d40515152f46db6ca67a6e375e7a
Connor James Leech <connorleech@gmail.com>
2014-05-02 10:35:53 +0200

    add transition effect to donut

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ two.js
 angular-d3 donut chart from @vicapow: http://vicapow.github.io/angular-d3-talk/slides/#/5
 
 vicapow donut source: https://github.com/vicapow/angular-d3-talk/tree/master/slides/demos/lets-make-a-donut-chart-directive
+
+pie chart update II: http://bl.ocks.org/mbostock/1346410
--- d3/js/donut.js
+++ d3/js/donut.js
@@ -28,7 +28,7 @@ var svg = d3.select('#target').append('svg')
 	.append('g')
 		.attr('transform', 'translate(' + width/2 + ',' + height/2 + ')');
 
-// draw path
+// draw every arc
 var arcs = svg.selectAll('path')
 	.data(pie(data['apples']))
 	.enter()
@@ -36,21 +36,22 @@ var arcs = svg.selectAll('path')
 		.attr('fill', function(d, i){
 			return color(i);	// color based on index
 		})
-		.attr('d', arc);
+		.attr('d', arc)
+		.each(function(d){ this.currentDatum = d; })	// store each path's data
 
 // add text labels
 var text = svg.selectAll('text')
 	.data(pie(data['apples']))
 	.enter()
-	.append('text')
-	.attr('transform', function(d){
-		return 'translate(' + arc.centroid(d) + ')';
-	})
-	.text('text-anchor', 'middle')
-	.attr('font-size', '2em')
-	.text(function(d){
-		return d.data;
-	})
+		.append('text')
+		.attr('transform', function(d){
+			return 'translate(' + arc.centroid(d) + ')';
+		})
+		.text('text-anchor', 'middle')
+		.attr('font-size', '2em')
+		.text(function(d){
+			return d.data;
+		});
 
 // listen for changes
 d3.selectAll('input').on('change', change);
@@ -59,14 +60,28 @@ function change(){
 	var value = this.value;	// this is selected <input> tag
 
 	arcs = arcs.data(pie(data[value]));	// supply new data
-	arcs.attr('d', arc);	// redraw arcs
-
+	//arcs.attr('d', arc);	// redraw arcs
+	arcs.transition().duration(750).attrTween('d', arcTween)
+	
 	// redraw labels
 	text.data(pie(data[value]))
+		.transition()
+		.delay(300)
 		.attr('transform', function(d){
 			return 'translate(' + arc.centroid(d) + ')';
 		})
 		.text(function(d){
 			return d.data;
 		})
+}
+
+// Store the displayed angles in currentDatum.
+// Then, interpolate from currentDatum to the new angles.
+// During the transition, currentDatum is updated in-place by d3.interpolate.
+function arcTween(a){
+	var i = d3.interpolate(this.currentDatum, a);
+	this.currentDatum = i(0)
+	return function(t){
+		return arc(i(t))
+	}
 }
